9c27395cfe128711e404e696ab2a079f6d4c3707
Jacob Johansson <Jacobjohansson@live.com>
2019-08-08 10:10:41 +0200

    Added Button

--- elements/Button/index.jsx
+++ elements/Button/index.jsx
@@ -1,135 +1,45 @@
 import React, { Component } from 'react';
 import './Button.scss';
-import PropTypes from 'prop-types';
 
-const propTypes = {
-    size: PropTypes.string,
-    variant: PropTypes.string,
-    disabled: PropTypes.bool,
-    active: PropTypes.bool,
-    className: PropTypes.string,
-    icon: PropTypes.string,
-    iconposition: PropTypes.string,
-    iconsize: PropTypes.string,
-    color: PropTypes.string
-};
+export const Button = (props) => {
+    const { variant, size, color, className, disabled, children, icon, iconPosition } = props;
+    let classString = 'Button';
 
-const defaultProps = {
-    size: '',
-    variant: '',
-    disabled: false,
-    active: false,
-    className: '',
-    iconposition: 'left',
-    iconsize: 'medium',
-    icon: null,
-    color: ""
-};
+    variant && variant.split(" ").forEach(val => {
+        classString += ` Button--variant--${val}`
+    });
 
-export class Button extends Component {
-    constructor(props) {
-        super(props);
-        this.allowediconsizes = ['small', 'medium', 'large'];
+    color && color.split(" ").forEach(val => {
+        classString += ` Button--color--${val}`
+    });
+
+    size && size.split(" ").forEach(val => {
+        classString += ` Button--size--${val}`
+    });
+
+    if(className){
+        classString += ` ${className}`
     }
 
-    getTypeClass(type) {
-        const classes = {
-            primary: ' Button--primary',
-            danger: ' Button--danger',
-            tab: ' Button--tab',
-            link: ' Button--link',
-            outline: ' Button--outline',
-            ghost: ' Button--ghost'
-        };
-
-        let className = "";
-        let parts = type.split(" ");
-        parts.forEach(part => {
-            className += classes[part] || '';
-        })
-
-        return className;
+    if(disabled){
+        classString += ` Button--disabled`;
     }
 
-    getSizeClass(size) {
-        const classes = {
-            large: ' Button--large',
-            small: ' Button--small',
-            'full-width': ' Button--expanded'
-        };
-
-        return classes[size] || '';
+    if(icon){
+        classString += ` Button--withIcon`
     }
 
-    getColorClass(size) {
-        const classes = {
-            white: ' Button--white'
-        };
+    let elementProps = Object.assign({}, props);
+    delete elementProps.className;
+    delete elementProps.active;
 
-        return classes[size] || '';
-    }
-
-    render() {
-        var classString = 'Button';
-        let icon = "";
-        if (this.props.loading) {
-            classString += ' Button--disabled';
-        }
-
-        classString += this.getSizeClass(this.props.size);
-        classString += this.getTypeClass(this.props.variant);
-        classString += this.getColorClass(this.props.color);
-
-        if (this.props.className) {
-            classString = classString + ' ' + this.props.className;
-        }
-        if (this.props.active) {
-            classString += ' Button--active';
-        }
-        if (this.props.disabled) {
-            classString += ' Button--disabled';
-        }
-
-        if (this.props.icon) {
-            if (this.allowediconsizes.indexOf(this.props.iconsize) < 0) {
-                this.props.iconsize = 'medium';
-            }
-            let iconClass = `Button__icon Button__icon--${this.props.iconsize}`;
-            icon = <img src={this.props.icon} alt={this.props.children.toString()} className={iconClass} />
-        }
-
-        let elementProps = Object.assign({}, this.props);
-        delete elementProps.className;
-        delete elementProps.active;
-
-        if (this.props.button) {
-            return (
-                <button className={classString} {...elementProps}>
-                    <React.Fragment>
-                        {this.props.iconposition === 'left' && icon ? icon : null}
-                        {this.props.loading ? 'Loading...' : this.props.children}
-                        {this.props.iconposition === 'right' && icon ? icon : null}
-                    </React.Fragment>
-                </button>
-            );
-        } else {
-            return (
-                <div className={classString} {...elementProps}>
-                    <React.Fragment>
-                        {this.props.iconposition === 'left' && icon ? icon : null}
-                        {this.props.loading ? 'Loading...' : this.props.children}
-                        {this.props.iconposition === 'right' && icon ? icon : null}
-                    </React.Fragment>
-                </div>
-            );
-        }
-       
-            
-        
-
-        
-    }
+    return (
+        <button className={classString} {...elementProps}>
+            <React.Fragment>
+                { !iconPosition || iconPosition === "left" ? icon : "" }
+                { children }
+                { iconPosition === "right" ? icon : "" }
+            </React.Fragment>
+        </button>
+    );
 }
-
-Button.propTypes = propTypes;
-Button.defaultProps = defaultProps;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
 import { Welcome } from '@storybook/react/demo';
 
 import "./button";
